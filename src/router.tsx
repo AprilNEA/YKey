6b767fe13ce2457bd84f09bf467137411b5346bb
AprilNEA <github@sku.moe>
2025-06-28 16:47:41 +0800

    feat: add tailwindcss

--- src/router.tsx
+++ src/router.tsx
@@ -1,19 +1,16 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import { invoke } from "@tauri-apps/api/core";
-import "./App.css";
+import "./index.css";
+import { Route, Router } from "wouter";
+
+import LandingPage from "./pages/index";
+import XKeyManager from "./pages/manager";
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
-
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    setGreetMsg(await invoke("greet", { name }));
-  }
-
   return (
-    <main className="container">YKey</main>
+    <Router>
+      <Route path="/" component={LandingPage} />
+      <Route path="/manager" component={XKeyManager} />
+      <Route path="/manager/:section" component={XKeyManager} />
+    </Router>
   );
 }
 
